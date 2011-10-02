123683bf927837fc9564dfb7d08d2967b492d6da
Stanislav Egarmin <ustas078@gmail.com>
2011-10-03 01:16:42 +0300

    ticket:3 Change LogRequest model

--- loginfo/models.py
+++ loginfo/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -5,8 +6,8 @@ from django.utils.translation import ugettext_lazy as _
 class LogRequest(models.Model):
     added = models.DateTimeField(auto_now_add=True, verbose_name=_('was added'))
     path = models.TextField(verbose_name=_('full path'))
-    is_secure = models.BooleanField(verbose_name=_('secured'))
-    is_ajax = models.BooleanField(verbose_name=_('via ajax'))
+    is_secure = models.CharField(max_length=6, verbose_name=_('secured'))
+    is_ajax = models.CharField(max_length=6, verbose_name=_('via ajax'))
     content = models.TextField(verbose_name=_('parameters'))
     user_info = models.TextField(verbose_name=_('user info'))
 
