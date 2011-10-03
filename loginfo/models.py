7d622d0e1f315cde416afa672d18f07bb287048e
Stanislav Egarmin <ustas078@gmail.com>
2011-10-03 15:44:03 +0300

    ticket:3 Remove some fields from model

--- loginfo/models.py
+++ loginfo/models.py
@@ -6,9 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 class LogRequest(models.Model):
     added = models.DateTimeField(auto_now_add=True,
                                  verbose_name=_('was added'))
-    path = models.TextField(verbose_name=_('full path'))
-    is_secure = models.CharField(max_length=6, verbose_name=_('secured'))
-    is_ajax = models.CharField(max_length=6, verbose_name=_('via ajax'))
+    path = models.TextField(verbose_name=_('path'))
     content = models.TextField(verbose_name=_('parameters'))
     user_info = models.TextField(verbose_name=_('user info'))
 
