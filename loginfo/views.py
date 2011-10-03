664c64eef82802ef4559b5b051bd4948762083a1
Stanislav Egarmin <ustas078@gmail.com>
2011-10-03 15:03:03 +0300

    ticket:3 add views.py

--- loginfo/views.py
+++ loginfo/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from personal.decorators import render_to
+from loginfo.models import LogRequest
+
+
+@render_to('display_req.html')
+def display_requests(request):
+    try:
+        req_list = LogRequest.objects.filter(pk__lt=11)
+    except:
+        req_list = None
+    return {'req_list':req_list}
