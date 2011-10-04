55d915bc3e78500b791c5a557db6ae1c169530b9
Stanislav Egarmin <ustas078@gmail.com>
2011-10-04 21:07:14 +0300

    ticket:3 Errors recovery#1

--- loginfo/views.py
+++ loginfo/views.py
@@ -6,7 +6,7 @@ from loginfo.models import LogRequest
 @render_to('display_req.html')
 def display_requests(request):
     try:
-        req_list = LogRequest.objects.filter(pk__lt=11)
+        req_list = LogRequest.objects.all().order_by('added')[0:10]
     except:
         req_list = None
     return {'req_list': req_list}
