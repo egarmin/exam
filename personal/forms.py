28a31a265b784976d434dff799f05072e17945c4
Stanislav Egarmin <ustas078@gmail.com>
2011-10-05 23:17:57 +0300

    ticket:6 Widget for date is adding

--- personal/forms.py
+++ personal/forms.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
-from django.forms.formsets import formset_factory
 
+class CalendarWidget(forms.TextInput):
+    """ Widget for DateFields
+    """
+    class Media:
+        js = ('/jsi18n',
+                settings.ADMIN_MEDIA_PREFIX + 'js/core.js',
+                settings.ADMIN_MEDIA_PREFIX + 'js/calendar.js',
+                settings.ADMIN_MEDIA_PREFIX + 'js/admin/DateTimeShortcuts.js')
+        css = {
+            'all': (
+                settings.ADMIN_MEDIA_PREFIX + 'css/widgets.css',
+            )
+        }
+
+    def __init__(self, attrs={}):
+        super(CalendarWidget, self).__init__(attrs={'class': 'vDateField',
+                                                    'size': '10'})
 
 class PersonForm(forms.Form):
     name = forms.CharField(max_length=50, label=_('Name'),
@@ -11,7 +28,8 @@ class PersonForm(forms.Form):
     surname = forms.CharField(max_length=50, label=_('Surname'),
                  error_messages={'required': _("Enter your surname, please.")})
     birthday = forms.DateField(input_formats=["%d.%m.%Y", "%Y-%m-%d"],
-                               label=_("Birthday"), required=False)
+                               label=_("Birthday"), required=False,
+                               widget=CalendarWidget)
     bio = forms.CharField(label=_('Biography'), required=False,
                     widget=forms.Textarea(attrs={'cols': '35', 'rows': '6'}))
 
