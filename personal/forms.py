7d73acc43ef36ea52e7f64e5a43e144c330cdb49
Stanislav Egarmin <ustas078@gmail.com>
2011-10-05 20:52:31 +0300

    ticket:6 Views.py with ajax was added

--- personal/forms.py
+++ personal/forms.py
@@ -7,9 +7,9 @@ from django.forms.formsets import formset_factory
 
 class PersonForm(forms.Form):
     name = forms.CharField(max_length=50, label=_('Name'),
-                 error_messages={'required': _("Enter your surname, please")})
+                 error_messages={'required': _("Enter your name, please.")})
     surname = forms.CharField(max_length=50, label=_('Surname'),
-                 error_messages={'required': _("Enter your surname, please")})
+                 error_messages={'required': _("Enter your surname, please.")})
     birthday = forms.DateField(input_formats=["%d.%m.%Y", "%Y-%m-%d"],
                                label=_("Birthday"), required=False)
     bio = forms.CharField(label=_('Biography'), required=False,
@@ -18,7 +18,8 @@ class PersonForm(forms.Form):
 
 class ContactForm(forms.Form):
     email = forms.EmailField(max_length=50, label=_("Email"), required=False)
-    jid = forms.EmailField(max_length=50, label=_('Jabber'), required=False)
+    jid = forms.EmailField(max_length=50, label=_('Jabber'), required=False,
+                  error_messages={'invalid': _("Enter a valid jabber ID.")})
     skype = forms.CharField(max_length=13, label=_('Skype'), required=False)
     appendix = forms.CharField(label=_('Appendix'), required=False,
                     widget=forms.Textarea(attrs={'cols': '35', 'rows': '6'}))
--- personal/views.py
+++ personal/views.py
@@ -3,10 +3,12 @@ from personal.models import Person, Contacts
 from personal.forms import PersonForm, ContactForm
 from django.utils.translation import ugettext_lazy as _
 from personal.decorators import render_to
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.forms.formsets import formset_factory
 from django.utils import simplejson as json
+from django.template import loader, RequestContext
+
 
 @render_to('display_pers.html')
 def display_person(request):
@@ -21,43 +23,48 @@ def display_person(request):
 
 @login_required
 def edit_person(request):
+    try:
+        pers = Person.objects.get(pk=1)
+        cont = Contacts.objects.get(person=pers)
+    except:
+        pers = None
+        cont = None
     if request.method == 'POST':
-        try:
-            pers = Person.objects.get(pk=1)
-            cont = Contacts.objects.get(person=pers)
-        except:
-            return HttpResponse('DB error')
+
+        ajax = request.is_ajax() # 
         c_form_set = formset_factory(Contacts, formset=ContactForm)
         p_form = PersonForm(request.POST)
         c_form = c_form_set(request.POST)
         is_c_valid = c_form.is_valid()
-        if p_form.is_valid() and is_c_valid:
+        if p_form.is_valid() and is_c_valid:# forms are correct
+            data = p_form.cleaned_data
+            pers.name = data['name']
+            pers.surname = data['surname']
+            pers.birthday = data['birthday']
+            pers.bio = data['bio']
+            data = c_form.cleaned_data
+            cont.jid = data['jid']
+            cont.skype = data['skype']
+            cont.appendix = data['appendix']
+            cont.email = data['email']
             try:
-                data = p_form.cleaned_data
-                pers.name = data['name']
-                pers.surname = data['surname']
-                pers.birthday = data['birthday']
-                pers.bio = data['bio']
-                data = c_form.cleaned_data
-                cont.jid = data['jid']
-                cont.skype = data['skype']
-                cont.appendix = data['appendix']
-                cont.email = data['email']
                 pers.save()
                 cont.save()
             except:
-                p_form.profilesave_error = _('Pers save error. Try later.')
-                c_form.profilesave_error = _('Cont save error. Try later.')
-            out = {}
+                p_form.save_error = _('Pers save error. Try later.')
+                c_form.save_error = _('Cont save error. Try later.')
+                if ajax:
+                    out = json.dumps({'status': 'FAIL',
+                           'common_nonform_errors': 'DB Saving error'})
+                    return HttpResponse(out, mimetype='application/json')
+            out = {'status': 'ok'}
         else:
-            out_f = dict()
-            out_f['p'] = p_form.errors
-            out_f['c'] = c_form.errors
-            out = json.dumps(out_f)
-        return HttpResponse(out, mimetype='application/json')
-    return HttpResponseRedirect('/')
-########################
-else:
+            out = {'status': 'FAIL',
+                   'pers_errors': p_form.errors,
+                   'cont_errors': c_form.errors}
+        if ajax:
+            return HttpResponse(json.dumps(out), mimetype='application/json')
+    else: #return values from DB
         p_form = PersonForm({'name': pers.name,
                            'surname': pers.surname,
                            'bio': pers.bio,
@@ -68,5 +75,6 @@ else:
                              'appendix': cont.appendix,
                              'email': cont.email
                            })
-    return {'person_form': p_form, 'contact_form': c_form}
-
+        tm = loader.get_template('edit_pers.html')
+        c = RequestContext(request, {'person_form': p_form, 'contact_form': c_form})
+        return HttpResponse(tm.render(c))
