2d92e4e7bd7992710c8796614506b86d2c9a17bd
Stanislav Egarmin <ustas078@gmail.com>
2011-10-06 17:34:22 +0300

    ticket:7 Reverse fields order in forms' init

--- personal/forms.py
+++ personal/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from personal.models import Person, Contacts
 
 
 class CalendarWidget(forms.TextInput):
@@ -23,7 +24,7 @@ class CalendarWidget(forms.TextInput):
                                                     'size': '12'})
 
         
-class PersonForm(forms.Form):
+class PersonForm(forms.ModelForm):
     name = forms.CharField(max_length=50, label=_('Name'),
                  error_messages={'required': _("Enter your name, please.")})
     surname = forms.CharField(max_length=50, label=_('Surname'),
@@ -34,10 +35,26 @@ class PersonForm(forms.Form):
                     widget=forms.Textarea(attrs={'cols': '35', 'rows': '6'}))
 
 
-class ContactForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(PersonForm, self).__init__(*args, **kwargs)
+        self.fields.keyOrder.reverse()
+
+    class Meta:
+        model = Person
+
+
+class ContactForm(forms.ModelForm):
     email = forms.EmailField(max_length=50, label=_("Email"), required=False)
     jid = forms.EmailField(max_length=50, label=_('Jabber'), required=False,
                   error_messages={'invalid': _("Enter a valid jabber ID.")})
     skype = forms.CharField(max_length=13, label=_('Skype'), required=False)
     appendix = forms.CharField(label=_('Appendix'), required=False,
                     widget=forms.Textarea(attrs={'cols': '35', 'rows': '6'}))
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields.keyOrder.reverse()
+
+    class Meta:
+        model = Contacts
+
