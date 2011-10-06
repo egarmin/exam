003116b41b19778c81bc3e5e0f67eb82c01d52db
Stanislav Egarmin <ustas078@gmail.com>
2011-10-06 22:56:26 +0300

    ticket:8 Own tag 'admin_tag' was added

--- personal/templatetags/owntag.py
+++ personal/templatetags/owntag.py
@@ -7,4 +7,16 @@ register = template.Library()
 
 @register.simple_tag
 def admin_link(arg):
-    return ' '
+    if hasattr(arg, '_meta'):
+        app = arg._meta.app_label
+        module = arg._meta.module_name
+    elif hasattr(arg, 'model'):
+        app = arg.model._meta.app_label
+        module = arg.model._meta.module_name
+    else:
+        error = "The elink's argument must be a model or QuerySet. %s isn't a model." % arg
+        raise template.TemplateSyntaxError(error)
+    res_link = '/admin/%s/%s/' % (app, module)
+    if hasattr(arg, 'id'):
+        res_link += '%s/' % arg.id
+    return res_link
