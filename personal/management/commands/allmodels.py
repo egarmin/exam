45cc8b19466a52746169ad622e50b0aa65b5bb3c
Stanislav Egarmin <ustas078@gmail.com>
2011-10-07 03:33:19 +0300

    ticket:9 Command 'allmodels' was added now

--- personal/management/commands/allmodels.py
+++ personal/management/commands/allmodels.py
@@ -6,4 +6,12 @@ class Command(BaseCommand):
     help = 'get list of models and its objects quantity'
 
     def handle(self, *args, **options):
-       pass
+        models_list = models.get_models()
+        self.stdout.write('There are %s models. Notably:\n' % len(models_list))
+        self.stderr.write('error: There are %s models. Notably:\n' % len(models_list))
+        for model in models_list:
+            n = model.objects.count()
+            model_name = str(model).split('\'')[1]
+            self.stdout.write('%s has %s object(s)\n' % (model_name, n))
+            self.stderr.write('error: %s has %s object(s)\n' % (model_name, n))
+
