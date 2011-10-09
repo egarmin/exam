1235ca04433441001ef71d5cc6ca495772bf570e
Stanislav Egarmin <ustas078@gmail.com>
2011-10-10 01:48:00 +0300

    ticket:13 Add ordered by priority field

--- loginfo/views.py
+++ loginfo/views.py
@@ -5,5 +5,5 @@ from loginfo.models import LogRequest
 
 @render_to('display_req.html')
 def display_requests(request):
-    req_list = LogRequest.objects.all().order_by('added')[:10]
+    req_list = LogRequest.objects.all().order_by('-priority', 'added')[:10]
     return {'req_list': req_list}
