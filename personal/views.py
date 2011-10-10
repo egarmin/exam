cb7a29ff86433f00f2fa496314b447c654a84218
Stanislav Egarmin <ustas078@gmail.com>
2011-10-10 16:50:14 +0300

    ticket:7 Fix pep8

--- personal/views.py
+++ personal/views.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.utils import simplejson as json
+
 from personal.models import Person
 from personal.forms import PersonForm
 from personal.decorators import render_to
-from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-from django.utils import simplejson as json
-from django.template import RequestContext
-from django.shortcuts import render_to_response
 
 
 @render_to('display_pers.html')
@@ -19,6 +20,7 @@ def display_person(request):
 
 
 @login_required
+@render_to('edit_pers.html')
 def edit_person(request):
     index = len(PersonForm.base_fields.keyOrder) / 2
     try:
@@ -36,11 +38,7 @@ def edit_person(request):
         if request.is_ajax():
                 return HttpResponse(json.dumps(out),
                                     mimetype='application/json')
-        return render_to_response('edit_pers.html',
-                              {'person_form': p_form, 'index': index},
-                              context_instance=RequestContext(request))
+        return {'person_form': p_form, 'index': index}
     else:
         p_form = PersonForm(instance=pers)
-        return render_to_response('edit_pers.html',
-                              {'person_form': p_form, 'index': index},
-                              context_instance=RequestContext(request))
+        return {'person_form': p_form, 'index': index}
