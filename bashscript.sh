6b7cc148ebfcfa3f3781f99ba5ea8c0ea39018ac
Stanislav Egarmin <ustas078@gmail.com>
2011-10-11 01:31:29 +0300

    ticket:13 Off south

--- bashscript.sh
+++ bashscript.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 prefix=`date +%F`
-path=/tmp/exam/
+path=/tmp/exam
 fname=$path/$prefix.dat
+curr=`pwd`
+echo curr = $curr
+echo old = $HOSTNAME
 mkdir -p /tmp/exam
 python manage.py allmodels 2>$fname
 echo Log stderr in \"$fname\" file.
--- settings.py
+++ settings.py
@@ -109,7 +109,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'personal',
     'loginfo',
-    'south',
+    #'south',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
