1150def83579a425c9c68690b335a54800baaed4
Stanislav Egarmin <ustas078@gmail.com>
2011-10-11 13:03:48 +0300

    master: Fix pep8 errors in loginfo/views

--- loginfo/views.py
+++ loginfo/views.py
@@ -2,6 +2,7 @@
 from loginfo.models import LogRequest
 from personal.decorators import render_to
 
+
 @render_to('display_req.html')
 def display_requests(request):
     req_list = LogRequest.objects.all().order_by('-priority', 'added')[:10]
