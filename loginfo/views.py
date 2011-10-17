3a9fce723643156c11f2f8b8b0453ec13a962488
Stanislav Egarmin <ustas078@gmail.com>
2011-10-18 01:13:02 +0300

    ticket#13 views end template was optimized

--- loginfo/views.py
+++ loginfo/views.py
@@ -4,37 +4,29 @@ from personal.decorators import render_to
 
 
 arr = {'pk': '&nbsp;',
-       'added': '&nbsp;',
+       'added': '&darr;',  # default
        'priority': '&nbsp;',
        'path': '&nbsp;',
        '&uarr;': '&darr;',
        '&darr;': '&uarr;',
-       'last': '',
-       'order': True}
+       'last': 'added',  # default
+       'order': False}   # default
 
 
 @render_to('display_req.html')
 def display_requests(request):
 
     main_field = request.GET.get('field')
-    req_list = LogRequest.objects.all().order_by('added')[:10]
-    if not main_field or main_field not in arr:
-        if arr['last'] != '':
-            arr[arr['last']] = '&nbsp;'
-            arr['last'] = ''
-    else:
-        sign = ['', '-']
+    sign = ['', '-']
+    if main_field in arr:
         if arr['last'] == main_field:
-            s = not arr['order']
-            arr['order'] = s
+            arr['order'] = not arr['order']
             arr[main_field] = arr[arr[main_field]]
         else:
-            s = False
             arr['order'] = False
-            if arr['last'] != '':
-                arr[arr['last']] = '&nbsp'
+            arr[arr['last']] = '&nbsp'
             arr[main_field] = '&darr;'
             arr['last'] = main_field
-        req_list = LogRequest.objects.all().order_by(sign[int(s)]
-                                               + main_field)[:10]
+    req_list = LogRequest.objects.all().order_by(sign[arr['order']]
+                                               + arr['last'])[:10]
     return {'req_list': req_list, 'arr': arr}
